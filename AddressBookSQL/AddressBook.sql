84ec2ee1e3f79471843629cb3627c1ed1694490d
ALLTALE2027 <86816300+ALLTALE2027@users.noreply.github.com>
2022-09-25 21:32:21 +0530

    [Darshan]: Add uc2 to create a table in DB

--- AddressBookSQL/AddressBook.sql
+++ AddressBookSQL/AddressBook.sql
@@ -1,2 +1,16 @@
 ﻿---------UC1------
 create database AddressBookService;
+use AddressBookService;
+------UC2---------
+create table AddressBook(
+firstName varchar(20) primary key,
+lastName varchar(20),
+address varchar(50),
+city varchar(20),
+state varchar(20),
+zip  int,
+phone bigint,
+email varchar(30)
+);
+
+select * from AddressBook;
